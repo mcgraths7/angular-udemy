754a427398378732a4437bf45700e0b96260714a
Steven McGrath <mcgrath.steven7@gmail.com>
2018-05-16 15:32:11 -0400

    Property binding and string interpolation

--- section-1/basics-assignment-1-start/src/app/warning-alert/warning-alert.component.ts
+++ section-1/basics-assignment-1-start/src/app/warning-alert/warning-alert.component.ts
@@ -13,3 +13,5 @@ export class WarningAlertComponent implements OnInit {
   }
 
 }
+
+
--- section-1/my-first-app/src/app/servers/servers.component.ts
+++ section-1/my-first-app/src/app/servers/servers.component.ts
@@ -2,14 +2,19 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`,
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServers = false;
 
-  constructor() { }
+  constructor() { setInterval(() => {
+      this.allowNewServers = !this.allowNewServers;
+    }, 2000);
+  }
 
   ngOnInit() {
   }
