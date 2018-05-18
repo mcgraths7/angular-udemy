1d66f2b2e8505fdca97a0a4dad4c75f659fb15f7
Steven McGrath <mcgrath.steven7@gmail.com>
2018-05-17 21:39:50 -0400

    Created ingredient model and added shopping list to component

---
+++ project/recipe-app/src/app/shared/ingredient.model.ts
@@ -0,0 +1,3 @@
+export class Ingredient {
+    constructor(public name: string, public measurement: string, public quantity: number) {  }
+}
--- project/recipe-app/src/app/shopping-list/shopping-list.component.ts
+++ project/recipe-app/src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredient.model';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+      new Ingredient('Chicken Thigh', 'pound(s)', 1.5),
+      new Ingredient('Carrots', 'bunch', 1),
+      new Ingredient('Red Potatoes', 'loose', 4),
+      new Ingredient('Curry Roux', 'cubes', 5),
+      new Ingredient('Garlic', 'loose', 6),
+      new Ingredient('Tortillas', 'loose', 6),
+      new Ingredient('Eggs', 'dozen', 1),
+      new Ingredient('Chicken Sausage', 'pound(s)' 2),
+      new Ingredient('Cheddar Cheese', 'pound(s)', 1),
+  ];
 
   constructor() { }
 
