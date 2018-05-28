72cf41b2c7e226936e4b1cf909f427f71bd55d62
Steven McGrath <mcgrath.steven7@gmail.com>
2018-05-28 13:37:44 -0400

    Started assignment 4

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -10,7 +10,8 @@ import { Component,
          AfterViewChecked,
          OnDestroy,
          ViewChild,
-         ElementRef} from '@angular/core';
+         ElementRef,
+         ContentChild} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -27,20 +28,34 @@ export class ServerElementComponent implements OnInit,
     };
     @Input() name;
     @ViewChild('heading') header: ElementRef;
+    @ContentChild('contentParagraph') paragraph: ElementRef;
 
   constructor() { console.log('Constructor Called'); }
 
-  ngOnChanges(changes: SimpleChanges): void { console.log('ngOnChanges called', changes); }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges called', changes);
+    console.log(changes);
+  }
 
-  ngOnInit() { console.log(this.header.nativeElement.textContent); }
+  ngOnInit() {
+    console.log('ngOnInit called');
+    console.log('Text Content of Header: ' + this.header.nativeElement.textContent);
+    console.log('Text Content of Paragraph: ' + this.paragraph.nativeElement.textContent);
+  }
 
   ngDoCheck() { console.log('ngDoCheck called'); }
 
   ngAfterContentInit() { console.log('ngAfterContentInit called'); }
 
-  ngAfterContentChecked() { console.log('ngAfterContentChecked called'); }
+  ngAfterContentChecked() {
+      console.log('ngAfterContentChecked called');
+      console.log('Text content of paragraph: ' + this.paragraph.nativeElement.textContent);
+  }
 
-  ngAfterViewInit() { console.log('ngAfterViewInit called', this.header.nativeElement.textContent); }
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit called');
+    console.log('Text content of Header: ' + this.header.nativeElement.textContent);
+  }
 
   ngAfterViewChecked() { console.log('ngAfterViewChecked called'); }
 
